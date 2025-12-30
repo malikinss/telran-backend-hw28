59fdcb8a3b41feb0f1fb2f8e868842eb082e17cb
Sam Malikin <workmalikin@gmail.com>
2025-12-30 20:38:13 +0200

    update __init__.py to expose RandomNumbersStream examples
    * Added `process_examples` to `__all__` for easy access to example usage.
    * Ensures users can import and run all random number stream examples directly.

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,9 @@
 # ./src/__init__.py
 
+from .random_numbers_stream import RandomNumbersStream
+from .examples_random_numbers_stream import process_examples
 
 __all__ = [
+    'RandomNumbersStream',
+    'process_examples'
 ]
